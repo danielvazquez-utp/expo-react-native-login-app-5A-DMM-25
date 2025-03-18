31f198d65bc0c959b21d190e9c43903d1ad1212a
Paulo Daniel Vázquez Mora <daniel.vazquez@utpuebla.edu.mx>
2025-03-18 13:06:03 -0600

    Archivo de barril de componentes agregado

---
+++ components/index.js
@@ -0,0 +1,21 @@
+import BackButton from "./BackButton"
+import Background from "./Background"
+import Button from "./Button"
+import { CustomDropDownList } from "./CustomDropDownList"
+import Header from "./Header"
+import { IonButton } from "./IonButton"
+import Logo from "./Logo"
+import Paragraph from "./Paragraph"
+import TextInput from "./TextInput"
+
+export { 
+    BackButton, 
+    Background, 
+    Button, 
+    CustomDropDownList,
+    Header,
+    IonButton,
+    Logo,
+    Paragraph,
+    TextInput
+}
